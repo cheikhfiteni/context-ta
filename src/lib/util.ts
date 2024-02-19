3b524ce8a9ea51f304c70ec33e4cbef7e81dfa02
cheikhfiteni <cheikhfiteni@gmail.com>
2024-02-19 18:11:12 -0500

    despite the bugs at least presentable rn

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -7,14 +7,24 @@ interface CBPosition {
     y: number;
 }
 
-// So the resizing isn't working rn to update pageHeight, and boundingRectHeight already does the trick, but keeping
-// this general structure of pageHeight
+// Keeping _pageHeight mechanism in params and App.tsx in case we need it to deal with resize. boundingRect should be fine though 
 export function calculateChatBoxPosition(position: ScaledPosition, _pageHeight: number): { x: number, y: number } {
-  const boundingOffset = (position.pageNumber - 1) * position.boundingRect.height;
+    const boundingOffset = (position.pageNumber - 1) * position.boundingRect.height;
+  
+    // Ignoring a known issue with the centering of the chat box and especially the tool tip for now
+    // TODO: these arbritrary resizing numbers will change, and dynamic resize with window
+    // See branch center-positioning-bug if curious
+  
+    const rightX = (position.boundingRect.width/2) + 700;
+    const leftX = (position.boundingRect.width/2) - 200;
 
-  const centerX = position.boundingRect.x1;
-  const centerY = position.boundingRect.y1 - 100;
-  console.log(`The pageNumber is: ${position.pageNumber}, The pageHeight inferred from boundingRectHeight is: ${position.boundingRect.height}, The boundingOffset is: ${boundingOffset}`);
+    // This looks a little better but I still think the initial demo looks best where it is relative or same place
+    // That you move around. To be cleaned
+    const chosenX = (Math.random() < 0.5 ? leftX : rightX)
 
-  return { x: centerX, y: (centerY + boundingOffset) } as CBPosition;
-}
+    const centerY = position.boundingRect.y1 - 100;
+  
+    console.log(`The pageNumber is: ${position.pageNumber}, The pageHeight inferred from boundingRectHeight is: ${position.boundingRect.height}, The boundingOffset is: ${boundingOffset}`);
+  
+    return { x: chosenX, y: (centerY + boundingOffset) } as CBPosition;
+  }
