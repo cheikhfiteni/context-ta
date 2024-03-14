f8866b5a1dc59a071fcc6d27ef934af0dcd687cf
cheikhfiteni <cheikhfiteni@gmail.com>
2024-03-14 12:53:25 -0400

    auth0 provider setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,33 @@
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { AUTH_CONFIG } from "./lib/auth0-provider";
+import history from "./lib/history";
 import { createRoot } from "react-dom/client";
+
+const onRedirectCallback = (appState) => {
+    history.push(
+      appState && appState.returnTo ? appState.returnTo : window.location.pathname
+    );
+  };
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(<App />);
+
+const providerConfig = {
+    domain: AUTH_CONFIG.domain,
+    clientId: AUTH_CONFIG.clientID,
+    onRedirectCallback,
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+      ...(AUTH_CONFIG.audience ? { audience: AUTH_CONFIG.audience } : null),
+    },
+  };
+
+
+root.render(
+<Auth0Provider
+    {...providerConfig}
+    >
+    <App />
+</Auth0Provider>
+);
